912d78a5032b5a3a0954d39f40a9ef2a3663013b
Abraham Williams <4braham@gmail.com>
2018-04-23 21:59:38 -0500

    Add tabs

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MatButtonModule, MatCardModule, MatListModule, MatToolbarModule } from '@angular/material';
+import { MatButtonModule, MatCardModule, MatListModule, MatTabsModule, MatToolbarModule } from '@angular/material';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
@@ -8,12 +8,14 @@ import { ServiceWorkerModule } from '@angular/service-worker';
 import { environment } from '../environments/environment';
 import { CodeBlockComponent } from './code-block/code-block.component';
 import { PwaStepComponent } from './pwa-step/pwa-step.component';
+import { PwaInstallComponent } from './pwa-install/pwa-install.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     CodeBlockComponent,
-    PwaStepComponent
+    PwaStepComponent,
+    PwaInstallComponent
   ],
   imports: [
     BrowserModule,
@@ -21,6 +23,7 @@ import { PwaStepComponent } from './pwa-step/pwa-step.component';
     MatButtonModule,
     MatCardModule,
     MatListModule,
+    MatTabsModule,
     MatToolbarModule,
     ServiceWorkerModule.register('/ngsw-worker.js', { enabled: environment.production })
   ],
---
+++ src/app/pwa-install/pwa-install.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'pwa-install',
+  templateUrl: './pwa-install.component.html',
+  styleUrls: ['./pwa-install.component.css']
+})
+export class PwaInstallComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
