17670e18dfde3014f62ff63d222d028c4a672579
Abraham Williams <4braham@gmail.com>
2018-04-26 22:03:49 -0500

    Set the groundwork for img-2

--- src/app/pwa-install/pwa-install.component.ts
+++ src/app/pwa-install/pwa-install.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'pwa-install',
@@ -7,9 +7,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PwaInstallComponent implements OnInit {
 
+  // private width = 896;
+  // private height = 571;
+  //
+  // @Input() imgWidth: number;
+  // @Input() imgHeight: number;
+
   constructor() { }
 
   ngOnInit() {
+    // requestAnimationFrame(() => {
+    //   this.imgWidth = this.containerWidth - 16;
+    //   this.imgHeight = this.imgWidth * ( this.height / this.width);
+    // });
   }
 
+  // private get containerWidth(): number {
+  //   return (document.querySelector('#container') as HTMLDivElement).offsetWidth;
+  // }
 }
