45a44a03d2e14b4625eef53994cbfad4ed8791dc
Abraham Williams <4braham@gmail.com>
2018-04-28 13:30:57 -0500

    Add manifest page content

--- src/app/pwa-install/pwa-install.component.ts
+++ src/app/pwa-install/pwa-install.component.ts
@@ -6,7 +6,6 @@ import { Component, OnInit, Input } from '@angular/core';
   styleUrls: ['./pwa-install.component.css']
 })
 export class PwaInstallComponent implements OnInit {
-
   private imgRatio = 821 / 1392;
   private defaultWidth = 960;
   private defaultHeight = this.defaultWidth * this.imgRatio;
--- src/app/pwa-manifest/pwa-manifest.component.ts
+++ src/app/pwa-manifest/pwa-manifest.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'pwa-manifest',
@@ -6,10 +6,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./pwa-manifest.component.css']
 })
 export class PwaManifestComponent implements OnInit {
+  private imgRatio = 1978 / 3104;
+  private defaultWidth = 960;
+  private defaultHeight = this.defaultWidth * this.imgRatio;
+
+  @Input() imgWidth: number;
+  @Input() imgHeight: number;
 
   constructor() { }
 
   ngOnInit() {
+    requestAnimationFrame(() => {
+      this.imgWidth = this.containerWidth - 16;
+      this.imgHeight = this.imgWidth * this.imgRatio;
+    });
   }
 
+  private get containerWidth(): number {
+    return (document.querySelector('#container') as HTMLDivElement).offsetWidth;
+  }
 }
