7a7502bacc43e893860cd6d77199a2061adec85e
Abraham Williams <4braham@gmail.com>
2020-07-29 08:31:34 -0500

    Update to angular 9

--- src/app/firebase/firebase.module.ts
+++ src/app/firebase/firebase.module.ts
@@ -2,9 +2,7 @@ import { CommonModule } from '@angular/common';
 import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
 import { MatListModule } from '@angular/material/list';
 import { RouterModule, Routes } from '@angular/router';
-import { CBComponent } from '../c-b/c-b.component';
-import { CodeBlockComponent } from '../code-block/code-block.component';
-import { StepComponent } from '../step/step.component';
+import { SharedModule } from '../shared/shared.module';
 import { FirebaseComponent } from './firebase.component';
 
 const routes: Routes = [
@@ -17,14 +15,12 @@ const routes: Routes = [
 @NgModule({
   declarations: [
     FirebaseComponent,
-    StepComponent,
-    CBComponent,
-    CodeBlockComponent
   ],
   imports: [
     CommonModule,
     MatListModule,
-    RouterModule.forChild(routes)
+    RouterModule.forChild(routes),
+    SharedModule,
   ],
   schemas: [CUSTOM_ELEMENTS_SCHEMA]
 })
