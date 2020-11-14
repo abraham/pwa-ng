157095d7abe740566a0c9b7fd0cc5baaf0244fcf
Abraham Williams <4braham@gmail.com>
2020-11-14 04:25:23 -0600

    Upgrade to Angular 11

--- src/app/c-b/c-b.component.spec.ts
+++ src/app/c-b/c-b.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { CBComponent } from './c-b.component';
 
@@ -6,11 +6,13 @@ describe('CBComponent', () => {
   let component: CBComponent;
   let fixture: ComponentFixture<CBComponent>;
 
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [CBComponent],
-    }).compileComponents();
-  }));
+  beforeEach(
+    waitForAsync(() => {
+      TestBed.configureTestingModule({
+        declarations: [CBComponent],
+      }).compileComponents();
+    }),
+  );
 
   beforeEach(() => {
     fixture = TestBed.createComponent(CBComponent);
--- src/app/firebase/firebase.component.spec.ts
+++ src/app/firebase/firebase.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { FirebaseComponent } from './firebase.component';
 
@@ -6,11 +6,13 @@ describe('FirebaseComponent', () => {
   let component: FirebaseComponent;
   let fixture: ComponentFixture<FirebaseComponent>;
 
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [FirebaseComponent],
-    }).compileComponents();
-  }));
+  beforeEach(
+    waitForAsync(() => {
+      TestBed.configureTestingModule({
+        declarations: [FirebaseComponent],
+      }).compileComponents();
+    }),
+  );
 
   beforeEach(() => {
     fixture = TestBed.createComponent(FirebaseComponent);
