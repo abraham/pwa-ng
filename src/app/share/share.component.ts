7bed2780c3ebb74237a4a873cedf3732557a7038
Abraham Williams <4braham@gmail.com>
2024-04-29 04:43:06 +0000

    npm run fix

--- src/app/share/share.component.ts
+++ src/app/share/share.component.ts
@@ -27,7 +27,10 @@ export class ShareComponent {
     },
   ];
 
-  constructor(private snackBar: MatSnackBar, private title: Title) {}
+  constructor(
+    private snackBar: MatSnackBar,
+    private title: Title,
+  ) {}
 
   private get currentUrl(): string {
     return window.location.href;
